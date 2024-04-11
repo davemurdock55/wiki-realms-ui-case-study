9b75d5106f9bf9762d53bced25d2c038df5e73fb
Sara Peel <sarampeel@gmail.com>
2024-04-11 15:28:27 -0600

    Intro adjusted

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,22 +4,26 @@ export default function Intro() {
   return (
     <div className="p-1 rounded-lg bg-fuchsia-300 drop-shadow-xl">
       <div className="p-3 bg-white rounded-lg">
-        <h2 className="mb-1 text-xl font-medium">Project Overview: Worldbuilding</h2>
+        <h2 className="mb-1 text-xl font-medium">Project Overview: Creating Worlds</h2>
         <p className="mb-3 text-sm">
           Worldbuilding is the creative process of constructing an imaginary world, often for storytelling in literature, film, games, or other media. It involves developing the
-          geography, history, cultures, languages, and ecology of a fictional setting to create a rich backdrop. This project originated as a way to record and organize a world, with all the
-          complexities that are included in such a process. Platform: A comprehensive Web App The web app was conceived after extensive user research and detailed design. Timeframe January-April 2024
-          Responsibilities and Accomplishments? We interviewed writers, worldbuilders, and common readers. We created iterative wireframe-level designs that evolved along the interview process. Though
-          the testing process is still ongoing, potential users expressed their anticipation for the final product.
+          geography, history, cultures, languages, and ecology of a fictional setting to create a rich backdrop. WikiRealms originated as a way to record and organize a world, with all the
+          complexities that are included in such a process.
         </p>
-        <h2 className="mb-1 text-xl font-medium">Platform: A comprehensive Web App</h2>
-        <p className="mb-3 text-sm">The web app was conceived after extensive user research and detailed design. </p>
-        <h2 className="mb-1 text-xl font-medium">Timeframe</h2>
-        <p className="mb-3 text-sm">January - April 2024</p>
-        <h2 className="mb-1 text-xl font-medium">Responsibilities and Accomplishments</h2>
-        <p className="mb-3 text-sm">
-          We interviewed writers, worldbuilders, and common readers. We created iterative wireframe-level designs that evolved along the interview process. Though the testing process is still ongoing,
-          potential users expressed their anticipation for the final product.
+        <div className="flex flex-row gap-x-2">
+          <h2 className="mb-1 text-xl font-medium pb-3">Platform:</h2>
+          <p className="align-bottom">Mobile Application</p>
+        </div>
+        <div className="flex flex-row gap-x-2">
+          <h2 className="mb-1 text-xl font-medium pb-3">Timeframe:</h2>
+          <p className="align-bottom">January - April 2024</p>
+        </div>
+        <h2 className="mb-1 text-xl font-medium">Accomplishments</h2>
+        <p className="my-3 text-sm">
+          Over the course of the WikiRealms project we followed an iterative process of user feedback, evolving
+          high-fidelity prototypes, and user experience mapping from all angles. We interviewed dozens of writers, 
+          worldbuilders, and common readers and created iterative wireframe-level designs that evolved along the 
+          interview process. Though the testing process is still ongoing, many potential users have expressed their anticipation for the final product.
         </p>
       </div>
     </div>
